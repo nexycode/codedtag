88d4c18b769ed5a9c829e801e06d678d353a3edc
Montasser Mossallem <nexycode@gmail.com>
2024-01-14 21:38:31 +0300

    u

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,20 +1,58 @@
 //https://www.npmjs.com/package/react-script-tag
-const { Component } = require("react");
- 
+
+import { Component } from "react"; 
+import Icons from './../options/icons';
+
+import './../assets/css/admin.min.css'
+
 class Dashboard extends Component {
      
 
     componentDidMount = () => {
-         
-
+        
     }
 
     render = () => {
         
         return (
-            <h1>
-                Hello World
-            </h1>
+            <>
+                <header className="wrapper white-bg border-bottom plr-0">
+                    <nav className="flexbox items-center offset-left offset-right plr-15 max-1150">
+                        <ul className="inline-list left-p-30 main-nav font-13 bold-list">
+                            <li>
+                                <a href='#' className="flexbox items-center">
+                                    <span className="flexbox items-center mr-5">
+                                        {Icons.view}
+                                    </span>
+                                    <span>
+                                        Visit Site
+                                    </span>
+                                </a>
+                            </li> 
+                        </ul>
+
+                        <ul className="inline-list left-p-30 main-nav font-13 bold-list offset-right">
+                            <li className="has-subitem">
+                                <a href='#' className="flexbox items-center make-bg-hover">
+                                   <span className="flexbox items-center img-thumb-cover">
+                                        <img src="https://webdeveloper.com/wp-content/uploads/2022/05/ManoelaIlic_Portrait-46x50.jpg" alt="Profile" />
+                                   </span> 
+                                </a>
+                                <div className="subitem-bas-on-click right-position">
+                                    <div>
+                                        <span>
+                                            Milicia David
+                                        </span>
+                                    </div>
+                                    <ul>
+                                        <li><a href='#'>Data</a></li>
+                                    </ul>
+                                </div>
+                            </li> 
+                        </ul>
+                    </nav>
+                </header>
+            </>
         );
 
     }
--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,17 @@ const gulpSass = require('gulp-sass');
 const sass = gulpSass( dartSass );
 
 var config = {
+
     css: {
         from: "./front-end/scss/**/*.scss",
         to:"./client/src/assets/css"
+    },
+
+    css_admin: {
+        from: "./front-end/admin-scss/**/*.scss",
+        to: "./client/src/assets/css"
     }
+
 }
 
 
@@ -28,9 +35,24 @@ gulp.task( "stylesheets.min", () => {
 });
 
 
+gulp.task( "admin.css.min", () => {
+
+    return gulp.src( config.css_admin.from )
+        .pipe( sass({ outputStyle: 'expanded' }) )
+        .pipe(uglifycss({
+            "maxLineLen": 0,
+            "uglyComments": true
+        }))
+        .pipe( concat( 'admin.min.css' ) )
+        .pipe(gulp.dest(config.css_admin.to));
+        
+});
+
+
 
 gulp.task( "watch",  () => {
       
     gulp.watch( config.css.from, gulp.parallel( "stylesheets.min" ) ); 
+    gulp.watch( config.css_admin.from, gulp.parallel( "admin.css.min" ) ); 
 
 });
