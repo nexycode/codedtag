4bfdd656e4973f0274f220485ee314802b442197
Montasser Mossallem <nexycode@gmail.com>
2024-01-15 21:27:04 +0300

    updates

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -25,31 +25,85 @@ class Dashboard extends Component {
                                         {Icons.view}
                                     </span>
                                     <span>
-                                        Visit Site
+                                        View Account
                                     </span>
                                 </a>
                             </li> 
                         </ul>
 
                         <ul className="inline-list left-p-30 main-nav font-13 bold-list offset-right">
+                            
+                            <li className="has-icon-only has-label">
+                                <a href="#">
+                                    <span className="flexbox items-center">
+                                        {Icons.notification}
+                                    </span>
+                                    <span className="flexbox items-center content-center">10</span>
+                                </a>
+                            </li>
+
                             <li className="has-subitem">
                                 <a href='#' className="flexbox items-center make-bg-hover">
                                    <span className="flexbox items-center img-thumb-cover">
                                         <img src="https://webdeveloper.com/wp-content/uploads/2022/05/ManoelaIlic_Portrait-46x50.jpg" alt="Profile" />
                                    </span> 
                                 </a>
-                                <div className="subitem-bas-on-click right-position">
-                                    <div>
+                                <div className="user-menu right-position">
+                                    <div className="user-name flexbox column-direction content-center">
+                                        <a href="#">
+                                            Lilian Micha'el
+                                        </a>
                                         <span>
-                                            Milicia David
+                                            @lilian
                                         </span>
                                     </div>
                                     <ul>
-                                        <li><a href='#'>Data</a></li>
+
+                                        <li>
+                                            <a href='#' className="flexbox items-center">
+                                                <span className="mr-5 flexbox">{Icons.articles}</span>    
+                                                <span>My Articles</span>
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href='#' className="flexbox items-center">
+                                                <span className="mr-5 flexbox">{Icons.question}</span>    
+                                                <span>My Questions</span>
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href='#' className="flexbox items-center">
+                                                <span className="mr-5 flexbox">{Icons.answer}</span>    
+                                                <span>My Answers</span>
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href='#' className="flexbox items-center">
+                                                <span className="mr-5 flexbox">{Icons.setting}</span>    
+                                                <span>Account Settings</span>
+                                            </a>
+                                        </li> 
                                     </ul>
+
+                                    <div className="flexbox pt-xsdsd logout-in-menu">
+                                        <a>
+                                            Logout
+                                        </a>
+                                    </div>
+
                                 </div>
+
+                                
                             </li> 
+
+                            
                         </ul>
+
+                        
+
                     </nav>
                 </header>
             </>
